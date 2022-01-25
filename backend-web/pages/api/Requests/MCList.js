eb75fe182ecafe69d974909a4314377ffcd598e2
Das-Horn <craigdoyle220301@gmail.com>
2022-01-25 15:00:56 +0000

    added sequelize documentation

--- backend-web/pages/api/Requests/MCList.js
+++ backend-web/pages/api/Requests/MCList.js
@@ -1,6 +1,19 @@
-import {sequelize, DataType} from 'sequelize';
+import {sequelize, DataType, Sequelize} from 'sequelize';
+import { isPrimitive } from 'sequelize/dist/lib/utils';
 
 export default function handler(req,res){
     // DB code init
+    const seq = new Sequelize('' ,'Uname', 'pass' , {
+        // host : , ip
+        // dialect: 'mysql'
+    });
+    try{
+        await seq.authenticate();
+        console.log('[MCLists] connection established to DB');
+    }catch (error){
+        console.log(`Cannot connect to the database`);
+    }
+    //Create a model of the table
     res.status(200);
 }
+
