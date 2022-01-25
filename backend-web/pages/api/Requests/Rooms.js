2c7c367364b0c7f5b860f3988f37fc561864d161
Das-Horn <craigdoyle220301@gmail.com>
2022-01-25 15:28:07 +0000

    Rooms api (Beta)

---
+++ backend-web/pages/api/Requests/Rooms.js
@@ -0,0 +1,40 @@
+import {Sequelize, DataTypes} from 'sequelize';
+
+export default function handler(req,res){
+    // DB code init
+    const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
+        host : process.env.IP,          // All these stats are defined in a .env file
+        dialect: 'mysql'
+    });
+    try{
+        await seq.authenticate();
+        console.log('[MCLists] connection established to DB');
+    }catch (error){
+        console.log(`Cannot connect to the database`);
+    }
+    //Create a model of the table
+    //create different one for each table
+    const Rooms = seq.define('Rooms', {      
+        RoomID : {
+            type : DataTypes.INTEGER
+        },
+        RoomName: {
+            type : DataTypes.STRINGS
+        }
+    },
+    {
+        timestamps : false
+    }
+    );
+    //sync DB
+    await Rooms.sync();
+    const results = await Rooms.findAll(); //request query
+    console.log(`[api MCLists] Results requested have been retrieved : \n${JSON.stringify(results)}`);
+    //close connection
+    seq.close();
+
+    res.status(200).json(results); //send results
+
+    res.status(200);
+}
+
