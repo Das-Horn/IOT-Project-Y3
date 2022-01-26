cc49433306dea8ce36a01a8dc9b2956b03063883
Das-Horn <craigdoyle220301@gmail.com>
2022-01-26 11:29:15 +0000

    Groundwork for more components

---
+++ backend-web/Components/Graph.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Graph extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
