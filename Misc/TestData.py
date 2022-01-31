45a8a200c7d7fce5e5512b681e84503f2f206450
Das-Horn <craigdoyle220301@gmail.com>
2022-01-31 12:13:15 +0000

    Redas login patch

--- Misc/TestData.py
+++ Misc/TestData.py
@@ -2,14 +2,14 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="10.147.19.13",
-  user="admin",
-  password="Mufcmufc@1",
+  user="craig",
+  password="mufcmufc",
   database="iotProject"
 )
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO SensorData (MC, DataType, Data, Room) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO SensorData (MC, DataType, Data , SensorID) VALUES (%s, %s, %s, %s)"
 val = ("1", "Temp", "20", "1")
 mycursor.execute(sql, val)
 
