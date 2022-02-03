a0e5c2fc71035af8b605e5155aca303d455a997c
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-03 15:11:27 +0000

    Api start for login

--- backend-web/Components/Login.js
+++ backend-web/Components/Login.js
@@ -5,21 +5,39 @@ export default class Login extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            error : null
+            error : null,
+            LoggedIn : false
         }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        const UName = document.querySelector('#UName').value;
+        const Pass = document.querySelector('#Pass').value;
+
+        fetch(`/api/Login/${UName}/${Pass}`)
+        .then(
+            (res) => {
+
+            }
+        )
     }
 
     render(){
-        return(
-            <div className={styles.Login}>
-                <div className={styles.LoginCont}>
-                    <h1>Login</h1>
-                    <input id="UName" placeholder="Username"></input>
-                    <input id="Pass" placeholder="Password"></input>
-                    {this.state.error != null ? <p>{this.state.error}</p> : ''}
-                    <button>Submit</button>
-                </div>    
-            </div>
-        );
+        if(this.state.LoggedIn){
+            return(<div></div>);
+        }else{
+            return(
+                <div className={styles.Login}>
+                    <div className={styles.LoginCont}>
+                        <h1>Login</h1>
+                        <input id="UName" placeholder="Username"></input>
+                        <input type="password" id="Pass" placeholder="Password"></input>
+                        {this.state.error != null ? <p>{this.state.error}</p> : ''}
+                        <button onClick={this.handleClick}>Submit</button>
+                    </div>    
+                </div>
+            );
+        }
     }
 }
---
+++ backend-web/pages/api/Login/[...args].js
@@ -0,0 +1,31 @@
+import {Sequelize, DataTypes} from 'sequelize';
+
+export default async function handler(req,res){
+    // DB code init
+    const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
+        host : process.env.IP,          // All these stats are defined in a .env file
+        dialect: 'mariadb'
+    });
+    try{
+        await seq.authenticate();
+        console.log('[Jobs] connection established to DB');
+    }catch (error){
+        console.log(`Cannot connect to the database`);
+    }
+    //Create a model of the table
+    //create different one for each table
+    const Login = seq.define('Login', {      
+
+    },
+    {
+        timestamps : false
+    }
+    );
+    //sync DB
+    const results = await Login.findAll(); //request query
+    console.log(`[api Jobs] Results requested have been retrieved : \n${JSON.stringify(results)}`);
+    //close connection
+    seq.close();
+
+    res.status(200).json(results); //send results
+}
