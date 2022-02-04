aeb7f692b088e9f5fc348298eaa848912eed8c2f
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-04 13:44:09 +0000

    prisma

--- backend-web/pages/api/Login/[...args].js
+++ backend-web/pages/api/Login/[...args].js
@@ -1,51 +1,80 @@
-import {Sequelize, DataTypes} from 'sequelize';
+import { PrismaClient } from '@prisma/client'
 
 export default async function handler(req,res){
-    // DB code init
-    const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
-        host : process.env.IP,          // All these stats are defined in a .env file
-        dialect: 'mariadb',
-        dialectOptions : {
-            options : {
-                requestTimeout : 4000
-            }
+    const prisma = new PrismaClient()
+    var args = req.query['args'];
+    
+    prisma.logins.findMany()
+    .then(
+        function(resp) {
+            var r = false;
+            resp.forEach(i => {
+                if(i.Name == args[0] && i.Password == args[1]){
+                    console.log('match')
+                    r = true;
+                }
+            });
+            return r;
         }
-    });
-    try{
-        await seq.authenticate();
-        console.log('[Login] connection established to DB');
-    }catch (error){
-        console.log(`Cannot connect to the database`);
-        res.status(500).json({error : "Timed out connecting to DB"});
-        return
-    }
-    //Create a model of the table
-    //create different one for each table
-    const Login = seq.define('Login', {
-        ID : {
-            type : DataTypes.INTEGER,
-            primaryKey: true
-        },
-        Name : {
-            type : DataTypes.STRING
-        },
-        Password : {
-            type: DataTypes.STRING
+    ).then(
+        function(resp) {
+            res.status(200).json({login : resp});
         }
-    },
-    {
-        timestamps : false
-    }
-    );
-    //sync DB
-    try{
-    const results = await Login.findAll(); //request query
-    console.log(`\n\n\n[api Login] Results requested have been retrieved : \n${JSON.stringify(results)}\n\n\n`);
-    //close connection
-    seq.close();
-
-    res.status(200).json(results); //send results
-    } catch(error){
-        res.status(500).json({error : error});
-    }
+    )
 }
+
+
+//Testing new DB stuff commenting this out in case of revert
+//
+// import {Sequelize, DataTypes} from 'sequelize';
+// import * as mariadb from 'mariadb';
+
+// export default async function handler(req,res){
+//     // DB code init
+//     const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
+//         host : process.env.IP,          // All these stats are defined in a .env file
+//         dialect: mariadb,
+//         dialectOptions : {
+//             options : {
+//                 requestTimeout : 4000
+//             }
+//         }
+//     });
+//     try{
+//         await seq.authenticate();
+//         console.log('[Login] connection established to DB');
+//     }catch (error){
+//         console.log(`Cannot connect to the database`);
+//         res.status(500).json({error : "Timed out connecting to DB"});
+//         return
+//     }
+//     //Create a model of the table
+//     //create different one for each table
+//     const Login = seq.define('Login', {
+//         ID : {
+//             type : DataTypes.INTEGER,
+//             primaryKey: true
+//         },
+//         Name : {
+//             type : DataTypes.STRING
+//         },
+//         Password : {
+//             type: DataTypes.STRING
+//         }
+//     },
+//     {
+//         timestamps : false
+//     }
+//     );
+//     //sync DB
+//     try{
+//     const results = await Login.findAll(); //request query
+//     console.log(`\n\n\n[api Login] Results requested have been retrieved : \n${JSON.stringify(results)}\n\n\n`);
+//     //close connection
+//     seq.close();
+
+//     res.status(200).json(results); //send results
+//     } catch(error){
+//         res.status(500).json({error : error});
+//     }
+// }
