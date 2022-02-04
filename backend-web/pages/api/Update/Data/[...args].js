ae4b5d8e224a53319c48fa164e1f3951e7f10522
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-04 22:18:47 +0000

    Update api's converted to prisma

--- backend-web/pages/api/Update/Data/[...args].js
+++ backend-web/pages/api/Update/Data/[...args].js
@@ -1,9 +1,9 @@
-import { Sequelize,DataTypes } from 'sequelize';
+import { PrismaClient } from '@prisma/client'
 
 
+export default async function handler(req,res){
+    const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
-    // Code to get the api variables from the link
     var args = req.query;
 
     if(args['args'].length != 4){
@@ -11,64 +11,16 @@ export default async function handler(req, res) {
         res.status(205).json('{"error" : "incorrect amount of arguments"}');
         return
     }
-    console.log(`[Debugging] link Variables : ${args['args']}`)
-    // Connect to DB and test connection
-    const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
-        host : process.env.IP,          // All these stats are defined in a .env file
-        dialect: 'mariadb'
-    });
-    try{
-      await seq.authenticate();
-      console.log('Connection Established');
-    }catch (error){
-      console.log('Unable to connect to DB:\t',error);
-    }
-    // Create Model (Table) and syn with DBc
 
-    const DataDB = seq.define('SensorData', {
-        MC : {
-            type: DataTypes.INTEGER
-        },
-        DataType: {
-            type: DataTypes.STRING
-        },
-        Data : {
-            type : DataTypes.STRING
-        },
-        TimeStamp : {
-            type : DataTypes.TIME
-        },
-        DataKey : {
-            type : DataTypes.INTEGER,
-            primaryKey : true
-        },
-        SensorID : {
-            type : DataTypes.INTEGER
+    const NewTypes = await prisma.sensorData.create({
+            data : {
+                MC : parseInt(args['args'][0]),
+                DataType : args['args'][1],
+                Data : args['args'][2],
+                SensorID : parseInt(args['args'][3]),
+            }
         }
-    },
-    {
-      timestamps: false
-    }
     );
-    // try to create a database entry
-    //Link format:
-    // localhost/api/Update/Data/MC/DataType/Data/SensorID
-    try {
-        await DataDB.create({
-            MC : parseInt(args['args'][0]),
-            DataType : args['args'][1],
-            Data : args['args'][2],
-            SensorID : parseInt(args['args'][3]),
-        });
-    } catch (error) {
-        console.log(`[Update] error adding entry to database`);
-        res.status(205).json({'OOPS' : error});
-        return;
-    }
-    const results = `${args['args'].join(' | ')}`
-    console.log(`[api Update] results of POST by user : \n${JSON.stringify(results,null, 2)}`);
-    // Close connection and send response
-    seq.close();
-    res.status(200).json(results);
-  }
-
+    console.log(NewTypes);
+    res.status(200).json(NewTypes);
+}
--- backend-web/pages/api/Update/jobs/[...args].js
+++ backend-web/pages/api/Update/jobs/[...args].js
@@ -1,65 +1,25 @@
-import { Sequelize,DataTypes } from 'sequelize';
+import { PrismaClient } from '@prisma/client'
 
 
-
-export default async function handler(req, res) {
-    // Code to get the api variables from the link
+export default async function handler(req,res){
+    const prisma = new PrismaClient();
     var args = req.query;
 
-    if(args['args'].length < 1){
+    if(args['args'].length < 2){
         console.log("Invalid variable amount");
         res.status(205).json('{"error" : "incorrect amount of arguments"}');
         return
     }
-    // Connect to DB and test connection
-    const seq = new Sequelize(process.env.DB ,process.env.UNAME, process.env.PASS , {
-        host : process.env.IP,          // All these stats are defined in a .env file
-        dialect: 'mariadb'
-    });
-    try{
-      await seq.authenticate();
-      console.log('Connection Established');
-    }catch (error){
-      console.log('Unable to connect to DB:\t',error);
-    }
-    // Create Model (Table) and syn with DBc
 
-    const Jobs = seq.define('Jobs', {
-        MC : {
-            type: DataTypes.INTEGER
+    const results = await prisma.jobs.update({
+        data : {
+            Action : parseInt(args['args'][1]) == 1 ? true : false
         },
-        JobID : {
-            type : DataTypes.INTEGER,
-            primaryKey : true
-        },
-        Action : {
-            type : DataTypes.BOOLEAN
-        },
-        SensorID : {
-            type : DataTypes.INTEGER
+        where : {
+            SensorID : parseInt(args['args'][0])
         }
-    },
-    {
-      timestamps: false
-    }
-    );
-    // try to create a database entry
-    try {
-        await Jobs.update({
-            Action : parseInt(args['args'][1]) == 1 ? true : false 
-        },
-        {
-            where : {SensorID : parseInt(args['args'][0])}
-        });
-    } catch (error) {
-        console.log(`[Update] error adding entry to database`);
-        res.status(205).json({OOPS : error});
-        return;
-    }
-    const results = `${args['args'].join(' | ')}`
-    console.log(`[api Update] results of POST by user : \n${JSON.stringify(results,null, 2)}`);
-    // Close connection and send response
-    seq.close();
+    })
+   
     res.status(200).json(results);
   }
 
