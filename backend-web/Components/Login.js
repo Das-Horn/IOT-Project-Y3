83be98a4f70984d9ddc443099aeeee884fcb1bbe
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-04 01:12:55 +0000

    vercel #3

--- backend-web/Components/Login.js
+++ backend-web/Components/Login.js
@@ -6,7 +6,7 @@ export default class Login extends React.Component{
         super(props);
         this.state = {
             error : null,
-            LoggedIn : true
+            LoggedIn : false
         }
         this.handleClick = this.handleClick.bind(this);
     }
