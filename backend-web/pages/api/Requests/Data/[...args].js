28510de2259fb6398c432e06a5695fb13fd82c95
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-09 20:18:56 +0000

    api adjustments for new graph

--- backend-web/pages/api/Requests/Data/[...args].js
+++ backend-web/pages/api/Requests/Data/[...args].js
@@ -4,18 +4,31 @@ const prisma = new PrismaClient();
 
 export default async function handler(req,res){
     const args = req.query['args'];
+    let results;
 
-
-
-    const results = await prisma.sensorData.findMany({
-        where : {
-            DataType : args[0]
-        },
-        orderBy : {
-            TimeStamp : "desc"
-        },
-        take : 100,
-    })
-    prisma.$disconnect()
+    if(args.length == 1){
+        results = await prisma.sensorData.findMany({
+            where : {
+                DataType : args[0]
+            },
+            orderBy : {
+                TimeStamp : "desc"
+            },
+            take : 100,
+        })
+    } else {
+        results = await prisma.sensorData.findMany({
+            where : {
+            AND: [
+                {DataType : args[0]},
+                {MC : args[1]}
+            ]
+            },
+            orderBy : {
+                TimeStamp : "desc"
+            },
+            take : 100,
+        })
+    }
     res.status(200).json(results);
 }
