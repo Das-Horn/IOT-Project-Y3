cbaf1e607103f859085209b33c02a6fddab9fce5
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-11 10:56:34 +0000

    phython humidity addition

--- Misc/TestData.py
+++ Misc/TestData.py
@@ -17,8 +17,16 @@ while True:
     i = str(random.randrange(0, 40))
     val = ("1", "Temp", i, "1")
     mycursor.execute(sql, val)
-    time.sleep(10)
     mydb.commit()
 
     print(mycursor.rowcount, "record inserted.")
     print("ID:", mycursor.lastrowid)
+
+    i = str(random.randrange(10, 100))
+    val = ("1", "Humid", i, "1")
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+    print("ID:", mycursor.lastrowid)
+    time.sleep(3)
