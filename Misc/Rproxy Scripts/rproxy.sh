64e3981ec9f326caff9ddfc6a21f58c9dbe349ba
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-02-15 13:07:25 +0000

    Script omits

--- Misc/Rproxy Scripts/rproxy.sh
+++ Misc/Rproxy Scripts/rproxy.sh
@@ -4,7 +4,7 @@ apt install python3
 systemctl stop apache2
 systemctl disable apache2
 curl -s https://install.zerotier.com | sudo bash
-zerotier-cli join 565799d8f6934700
+zerotier-cli join (ID HERE)
 python3 nginx-setup.py
 unlink /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
